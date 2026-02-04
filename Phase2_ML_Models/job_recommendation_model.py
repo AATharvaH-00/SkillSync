006468a2ad45fecd3656682ee46d7efd56c4a719
AATharvaH-00 <ashingmire42@gmail.com>
2026-02-04 23:10:04 +0530

    adding new python file

---
+++ Phase2_ML_Models/job_recommendation_model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import MultiLabelBinarizer
+import joblib
+import os
+from typing import List, Dict, Any
